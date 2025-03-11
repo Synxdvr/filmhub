1711f7156ba98641a4adffa5b3ad6a24f641f1b3
Tib <142139978+Synxdvr@users.noreply.github.com>
2025-03-11 12:33:06 +0800

    feat(APIConfig.kt): add api config with api key and url for tmdb

---
+++ app/src/main/java/com/example/filmhub/utils/APIConfig.kt
@@ -0,0 +1,7 @@
+package com.example.filmhub.utils
+
+object APIConfig {
+    const val API_KEY = "47bbc993148ad1fd29e81223fe9e7f57"
+    const val POSTER_URL = "https://image.tmdb.org/t/p/w500"
+    const val TMDB_URL = "https://api.themoviedb.org/3/"
+}
